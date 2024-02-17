78e2244eaa712ff1a69c6aa46ad27757d3d9feb3
Pablo Di Loreto <pdiloreto@puertotec.com>
2024-02-17 10:36:09 -0300

    Script para instalación de IIS y configuración de Deafult Website escuchando en puerto 81 desde Custom Script Extension de Azure VM

--- APT24A-Redes/Install-IIS.ps1
+++ APT24A-Redes/Install-IIS.ps1
@@ -1,2 +1,9 @@
+# Instalar el rol de IIS
 Add-WindowsFeature Web-Server
+
+# Cambiar el puerto del sitio web predeterminado a 81
+Import-Module WebAdministration
+Set-ItemProperty 'IIS:\Sites\Default Web Site' -Name bindings -Value @{protocol="http";bindingInformation="*:81:"}
+
+# Agregar el nombre del equipo al archivo Default.htm
 Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value $env:computername
